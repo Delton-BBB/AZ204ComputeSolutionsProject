23d048be5cb96ed365aa69c4c4b9a6ce82faefc6
Delton Fernandes <Delton.Fernandes@british-business-bank.co.uk>
2024-07-04 10:57:36 +0100

    added weather api project

---
+++ WeatherAPI/Controllers/WeatherAPIController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WeatherAPI.Controllers
+{
+    [Route("weatherapi")]
+    public class WeatherAPIController : Controller
+    {
+        WeatherAPIService _weatherAPIService;
+
+        public WeatherAPIController() { 
+            _weatherAPIService = new WeatherAPIService();
+        }
+
+        // GET: WeatherAPIController
+        [Route("current/{postcode}")]
+        public async Task<ActionResult> GetCurrentTemp(string postcode)
+        {
+            string jsonRes = await _weatherAPIService.GetCurrentWeatherByPostCode(postcode);
+
+            return Ok(jsonRes);
+        }
+
+/*        [Route("get")]
+        // GET: WeatherAPIController/Details/5
+        public ActionResult (int id)
+        {
+            return Ok("get");
+        }*/
+
+
+/*
+        // GET: WeatherAPIController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WeatherAPIController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WeatherAPIController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WeatherAPIController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WeatherAPIController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WeatherAPIController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }*/
+    }
+}
---
+++ WeatherAPI/Controllers/WeatherAPIService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+
+namespace WeatherAPI.Controllers
+{
+    public class WeatherAPIService
+    {
+
+        private string _subkey;
+        private string _url;
+        private string _endpoint;
+        private HttpClient _httpClient;
+
+        public WeatherAPIService() {
+
+            _subkey = "ce059e54d6aa445e949162734240307";
+            _url = "https://api.weatherapi.com/v1/";
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetCurrentWeatherByPostCode(string postCode) {
+
+            _endpoint = "current.json";
+
+            HttpResponseMessage JsonResult = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, _url+_endpoint+"?q="+postCode+"&key="+_subkey));            
+            return await JsonResult.Content.ReadAsStringAsync();
+        }
+
+    }
+}
